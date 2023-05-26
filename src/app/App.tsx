ea202455086fd0e9e17e50d4c31575f99cb8a58d
aleksei-gazov <lekha.gazov@yandex.ru>
2023-05-26 19:28:45 +0300

    test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -25,7 +25,7 @@ function App() {
     }, []);
 
     return (
-        <div className="App">gggg
+        <div className="App">ggggccccccccccccccccccccccc
             <Header/>
             {/*{isLoading && <h1>Loader...</h1>}*/}
             <Navigate/>
